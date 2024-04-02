a0dc481ad08cc91e446abd5ac218721cfd47575d
Jang Gyeoungguk <97073355+rnr9928@users.noreply.github.com>
2024-04-02 22:23:39 +0900

    지오메트리 도형 추가

--- src/src/3d.jsx
+++ src/src/3d.jsx
@@ -9,28 +9,18 @@ export default function Mythree(){
         const refMesh = useRef()
         const refWireMesh = useRef()
 
-        const {  xSize,
-            ySize,
-            zSize,
-            xSegments,
-            ySegments,
-            zSegments,} = useControls({
-                xSize: {value:1,min:0.1, max:5, step:0.01},
-                ySize: {value:1,min:0.1, max:5, step:0.01},
-                zSize: {value:1,min:0.1, max:5, step:0.01},
-                xSegments: {value:1,min:1, max:10, step:1},
-                ySegments: {value:1,min:1, max:10, step:1},
-                zSegments: {value:1,min:1, max:10, step:1}
+        const { radius,widthSegments,heightSegments,phiStart,phiLength } = useControls({
+               radius:{ value: 1, min:1, max:5,step:0.01,},
+               widthSegments:{value:32, min:3, max:256, step:1},
+               heightSegments:{value:32, min:2, max:256, step:1},
+               phiStart:{value:0, min:0, max:360, step:0.1},
+               phiLength:{value:360, min:0, max:360, step:0.1},
+
             })
             
         useEffect(()=>{
             refWireMesh.current.geometry = refMesh.current.geometry
-        },[xSize,
-            ySize,
-            zSize,
-            xSegments,
-            ySegments,
-            zSegments])
+        },[radius,widthSegments,heightSegments,phiLength,phiStart])
 
    
     return (
@@ -40,16 +30,8 @@ export default function Mythree(){
         <OrbitControls/>
 
         <mesh ref={refMesh} >
-            <boxGeometry args={
-                [
-                xSize,
-                ySize,
-                zSize,
-                xSegments,
-                ySegments,
-                zSegments
-            ]
-                }/>
+            <sphereGeometry args={[radius, widthSegments,heightSegments,phiStart * Math.PI/180,
+            phiLength* Math.PI/180]}/>
             <meshStandardMaterial color='#e67e22'/>
         </mesh>
         <mesh ref={refWireMesh} >
